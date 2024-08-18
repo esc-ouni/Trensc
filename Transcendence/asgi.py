3d6ca80efbb514896f0fec7fc3bc5c228fa57548
esc-ouni <idouni@student.1337.ma>
2024-08-18 11:40:01 +0100

    Test/updated

--- Transcendence/asgi.py
+++ Transcendence/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Trensendece project.
+ASGI config for Transcendence project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Trensendece.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Transcendence.settings')
 
 application = get_asgi_application()
--- Transcendence/wsgi.py
+++ Transcendence/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Trensendece project.
+WSGI config for Transcendence project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Trensendece.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Transcendence.settings')
 
 application = get_wsgi_application()
